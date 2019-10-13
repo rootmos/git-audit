45fa8276ae64f38ae4bebba58c385b2795f68c50
Gustav Behm <me@rootmos.io>
2019-10-13 02:59:07 +0200

    Test anchoring two commits

--- tests/test_git_audit.py
+++ tests/test_git_audit.py
@@ -9,67 +9,95 @@ import random
 
 from . import fresh, w3, faucets, ethereum_rpc_target, normalize
 
-def run(args, cwd):
-    subprocess.check_call(
-        [os.getenv("GIT_AUDIT_EXE")] + args,
-        cwd=cwd,
-        env={
-            "RUST_LOG": "git_audit",
-            "RUST_BACKTRACE": os.getenv("RUST_BACKTRACE", default=""),
-        }
-    )
+class test_env:
+    def __enter__(self):
+        self.exe = os.getenv("GIT_AUDIT_EXE")
+        self.root = tempfile.TemporaryDirectory()
+        self.global_config = os.path.join(self.root.name, "global.json")
+        self.repo = pygit2.init_repository(os.path.join(self.root.name, "repo"))
 
-def mk_global_config(d):
-    c = {
-        "ethereum": {
-            "private_key": faucets.key().hex(),
-            "rpc_target": ethereum_rpc_target,
-            "chain_id": w3.eth.chainId,
+        self.config = {
+            "ethereum": {
+                "private_key": faucets.key().hex(),
+                "rpc_target": ethereum_rpc_target,
+                "chain_id": w3.eth.chainId,
+            }
         }
-    }
-    p = os.path.join(d, "global.json")
-    with open(p, "w") as f:
-        f.write(json.dumps(c))
-    return p
+
+        with open(self.global_config, "w") as f:
+            f.write(json.dumps(self.config))
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.root.cleanup()
+
+    def run(self, args):
+        subprocess.check_call(
+            [self.exe, f"--global-config={self.global_config}"] + args,
+            cwd=self.repo.path,
+            env={
+                "RUST_LOG": "git_audit",
+                "RUST_BACKTRACE": os.getenv("RUST_BACKTRACE", default=""),
+            }
+        )
+
+    def commit(self):
+        try:
+            parents = [self.repo.head.target]
+        except:
+            parents = []
+
+        return self.repo.create_commit(
+            "refs/heads/master",
+            fresh.author(), fresh.committer(),
+            fresh.commit_msg(),
+            self.repo.index.write_tree(), parents
+        )
+
+    def inspect(self):
+        return GitAudit(self.repo.path)
+
+class GitAudit:
+    def __init__(self, path):
+        self.path = path
+
+        with open(os.path.join(path, ".git-audit.json")) as f:
+            c = json.loads(f.read())
+
+        self.contract = w3.eth.contract(
+            address = normalize(c["contract"]["address"]),
+            abi = c["contract"]["abi"],
+        )
+
+    @property
+    def commits(self):
+        res = self.contract.functions.commits().call()
+        return list(map(lambda c: c.to_bytes(20, "big"), res))
 
 class GitAuditTests(unittest.TestCase):
     def test_init(self):
-        with tempfile.TemporaryDirectory() as d:
-            gc = mk_global_config(d)
-            r = pygit2.init_repository(os.path.join(d, "repo"))
-            run([f"--global-config={gc}", "init"], cwd=r.path)
-
-            with open(os.path.join(r.path, ".git-audit.json")) as f:
-                c = json.loads(f.read())
-
-            contract = normalize(c["contract"]["address"])
-            self.assertNotEqual(len(w3.eth.getCode(contract)), 0)
+        with test_env() as te:
+            te.run(["init"])
+            self.assertNotEqual(len(w3.eth.getCode(te.inspect().contract.address)), 0)
 
     def test_anchor(self):
-        with tempfile.TemporaryDirectory() as d:
-            gc = mk_global_config(d)
-            r = pygit2.init_repository(os.path.join(d, "repo"))
-            tree = r.index.write_tree()
-            run([f"--global-config={gc}", "init"], cwd=r.path)
+        with test_env() as te:
+            te.run(["init"])
 
-            with open(os.path.join(r.path, ".git-audit.json")) as f:
-                c = json.loads(f.read())
+            c0 = te.commit()
+            te.run(["anchor"])
 
-            contract = w3.eth.contract(
-                address = normalize(c["contract"]["address"]),
-                abi = c["contract"]["abi"],
-            )
+            self.assertEqual(te.inspect().commits, [c0.raw])
 
-            c0 = r.create_commit(
-                "refs/heads/master",
-                fresh.author(), fresh.committer(),
-                fresh.commit_msg(),
-                tree, []
-            )
+    def test_anchor_twice(self):
+        with test_env() as te:
+            te.run(["init"])
 
-            run([f"--global-config={gc}", "anchor"], cwd=r.path)
+            c0 = te.commit()
+            te.run(["anchor"])
 
-            cs = list(map(lambda c: c.to_bytes(20, "big"),
-                contract.functions.commits().call()))
+            c1 = te.commit()
+            te.run(["anchor"])
 
-            self.assertEqual(cs, [c0.raw])
+            self.assertEqual(te.inspect().commits, [c0.raw, c1.raw])
